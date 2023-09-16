c5557aee16dbc8898564c65a56b187e8f179305e
SolomiaKukharuk <144213626+SolomiaKukharuk@users.noreply.github.com>
2023-09-16 23:59:08 +0300

    Update 2.10.py

--- 2.10.py
+++ 2.10.py
@@ -1,16 +1,10 @@
-x = abs(int(input()))
-a = x % 10
-b = (x % 100) // 10
-c = x // 100
-if a > b and a > c:
-	print (a)
+x = int(input())
+a = x // 100
+b = x % 10
+if a > b:
+    print(a)
 else:
-	pass 
-if b > a and b > c:
-	print(b)
-else:
-	pass 
-if c > b and c > a:
-	print(c)
-else:
-	pass 
+    if a < b:
+        print(b)
+    else:
+        print("=")
